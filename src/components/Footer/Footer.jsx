c1676253fc5d057722f777206b8b0bb87d2acee1
Darina202 <138057834+Darina202@users.noreply.github.com>
2026-03-28 18:24:24 +0200

    connect fonts and svg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,41 @@
 import styles from './footer.module.css';
-
+import sprite from '../../img/icons.svg';
 const Footer = () => {
   return (
     <div>
+      <a className={styles.logo} href="./">
+        <span className={styles.icon}>
+          <svg width="20" height="20">
+            <use href={`${sprite}#icon-logo`}></use>
+          </svg>
+        </span>
+        Food Boutique
+      </a>
+      <ul>
+        <li>
+          <a href="">
+            <svg className={styles.style} width="28" height="28">
+              <use href={`${sprite}#icon-facebook`} />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <svg className={styles.style} width="28" height="28">
+              <use href={`${sprite}#icon-instagram`} />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <svg className={styles.style} width="28" height="28">
+              <use href={`${sprite}#icon-youtube`} />
+            </svg>
+          </a>
+        </li>
+      </ul>
       <p>Logo</p>
+
       <p>Description</p>
       <p>Email</p>
     </div>
